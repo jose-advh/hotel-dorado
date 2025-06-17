d0782627829b8134659b8e8e545ed9bc10fa3e3c
jose-advh <josearmandodiazhernandez1@gmail.com>
2025-06-17 18:13:53 -0500

    Iframe 360 realizado, header mejorado

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,9 @@ const lugares = document.getElementById("lugares");
 const listaLugares = document.getElementById("listaLugares");
 const iconMenuMobile = document.getElementById("iconMenuMobile");
 const menuOverlay = document.getElementById("menuOverlay");
+const iframe360 = document.getElementById("iframe360");
+const buttonClose360 = document.getElementById("buttonClose360");
+const abrirIframe360 = document.getElementById("abrirIframe360");
 const listaDesplegable = document.querySelectorAll(".list__down");
 // Funcionalidades
 
@@ -22,6 +25,15 @@ function ocultarListaSiNoHayHover() {
 lugares.addEventListener("mouseenter", mostrarLista);
 lugares.addEventListener("mouseleave", ocultarListaSiNoHayHover);
 listaLugares.addEventListener("mouseleave", ocultarListaSiNoHayHover);
+abrirIframe360.addEventListener("click", () => {
+  iframe360.classList.remove("display-none");
+  buttonClose360.classList.remove("display-none");
+});
+
+buttonClose360.addEventListener("click", () => {
+  iframe360.classList.add("display-none");
+  buttonClose360.classList.add("display-none");
+});
 
 iconMenuMobile.addEventListener("click", () => {
   menuOverlay.classList.toggle("active");
