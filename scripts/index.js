e6945bb50f612d6788fd3a1ec827cff4d8ad7cb9
Jose <josearmandodiazh3@gmail.com>
2025-06-18 11:34:47 -0500

    Pagina habitaciones estructurada

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 // Variables:
-
 const lugares = document.getElementById("lugares");
 const listaLugares = document.getElementById("listaLugares");
 const iconMenuMobile = document.getElementById("iconMenuMobile");
@@ -8,38 +7,50 @@ const iframe360 = document.getElementById("iframe360");
 const buttonClose360 = document.getElementById("buttonClose360");
 const abrirIframe360 = document.getElementById("abrirIframe360");
 const listaDesplegable = document.querySelectorAll(".list__down");
-// Funcionalidades
 
+// Funcionalidades
 function mostrarLista() {
-  listaLugares.classList.remove("display-none");
+  if (listaLugares) listaLugares.classList.remove("display-none");
 }
 
 function ocultarListaSiNoHayHover() {
   setTimeout(() => {
-    if (!lugares.matches(":hover") && !listaLugares.matches(":hover")) {
+    if (
+      lugares &&
+      listaLugares &&
+      !lugares.matches(":hover") &&
+      !listaLugares.matches(":hover")
+    ) {
       listaLugares.classList.add("display-none");
     }
   }, 100);
 }
 
-lugares.addEventListener("mouseenter", mostrarLista);
-lugares.addEventListener("mouseleave", ocultarListaSiNoHayHover);
-listaLugares.addEventListener("mouseleave", ocultarListaSiNoHayHover);
-abrirIframe360.addEventListener("click", () => {
-  iframe360.classList.remove("display-none");
-  buttonClose360.classList.remove("display-none");
-});
+if (lugares && listaLugares) {
+  lugares.addEventListener("mouseenter", mostrarLista);
+  lugares.addEventListener("mouseleave", ocultarListaSiNoHayHover);
+  listaLugares.addEventListener("mouseleave", ocultarListaSiNoHayHover);
+}
 
-buttonClose360.addEventListener("click", () => {
-  iframe360.classList.add("display-none");
-  buttonClose360.classList.add("display-none");
-});
-
-iconMenuMobile.addEventListener("click", () => {
-  menuOverlay.classList.toggle("active");
-  listaDesplegable.forEach((lista) => {
-    setTimeout(() => {
-      lista.classList.toggle("display-none");
-    }, 500);
+if (abrirIframe360 && iframe360 && buttonClose360) {
+  abrirIframe360.addEventListener("click", () => {
+    iframe360.classList.remove("display-none");
+    buttonClose360.classList.remove("display-none");
   });
-});
+
+  buttonClose360.addEventListener("click", () => {
+    iframe360.classList.add("display-none");
+    buttonClose360.classList.add("display-none");
+  });
+}
+
+if (iconMenuMobile && menuOverlay && listaDesplegable.length > 0) {
+  iconMenuMobile.addEventListener("click", () => {
+    menuOverlay.classList.toggle("active");
+    listaDesplegable.forEach((lista) => {
+      setTimeout(() => {
+        lista.classList.toggle("display-none");
+      }, 500);
+    });
+  });
+}
